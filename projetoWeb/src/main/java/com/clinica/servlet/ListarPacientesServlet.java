e9948976f48730162de1fdcf0bd79046e3615b4a
M4rky07 <juniorgomes22919@gmail.com>
2026-02-05 10:21:35 -0300

    add servlet remover e listar.

--- projetoWeb/src/main/java/com/clinica/servlet/ListarPacientesServlet.java
+++ projetoWeb/src/main/java/com/clinica/servlet/ListarPacientesServlet.java
@@ -1,5 +1,36 @@
 package com.clinica.servlet;
 
-public class ListarPacientesServlet {
+import java.io.IOException;
+import java.util.List;
+
+import com.clinica.dao.PacienteDAO;
+import com.clinica.model.Paciente;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/listarPacientes")
+public class ListarPacientesServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        try {
+            PacienteDAO dao = new PacienteDAO();
+            List<Paciente> pacientes = dao.listar();
+
+            req.setAttribute("listaPacientes", pacientes);
+
+            req.getRequestDispatcher("pacientes-lista.jsp")
+               .forward(req, resp);
+
+        } catch (Exception e) {
+            throw new ServletException("Erro ao listar pacientes", e);
+        }
+    }
     
 }
--- projetoWeb/src/main/java/com/clinica/servlet/RemoverPacienteServlet.java
+++ projetoWeb/src/main/java/com/clinica/servlet/RemoverPacienteServlet.java
@@ -1,5 +1,33 @@
 package com.clinica.servlet;
 
-public class RemoverPacienteServlet {
-    
+import java.io.IOException;
+
+import com.clinica.dao.PacienteDAO;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/RemoverPaciente")
+public class RemoverPacienteServlet extends HttpServlet {
+
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String cpf = req.getParameter("cpf");
+
+        try {
+            PacienteDAO dao = new PacienteDAO();
+            dao.remover(cpf);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        resp.sendRedirect("paciente?acao=listar");
+    }
 }
